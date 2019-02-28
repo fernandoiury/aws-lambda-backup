ea021e1dc8fc8d664009af9c68f0f3612efc3201
Fernando Costa <fernando.costa@paytm.com>
2019-02-28 12:44:24 -0500

    Loop through all regions / Add permission to describe regions

--- ebs-backup.py
+++ ebs-backup.py
@@ -22,27 +22,35 @@ VERSION = '1.0.1'
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-ec2 = boto3.resource('ec2')
-client = boto3.client('ec2')
 
 
 def lambda_handler(event, context):
-    logger.info('Start ebs-backup v{}'.format(VERSION))
-    backup()
-    expire()
+    ec2 = boto3.resource('ec2')
+    client = boto3.client('ec2')
+    regions = client.describe_regions().get('Regions',[] )
+    for region in regions:
+        reg=region['RegionName']
+        logger.info('Checking Region {}'.format(reg))
+
+        ec2 = boto3.resource('ec2', region_name=reg)
+        client = boto3.client('ec2', region_name=reg)
+
+        logger.info('Start ebs-backup v{}'.format(VERSION))
+        backup(ec2, client)
+        expire(ec2)
 
 
-def backup():
+def backup(ec2, client):
     instances = ec2.instances.filter(Filters=[{'Name': 'tag-key',
                                                'Values': ['LambdaBackupConfiguration']}])
     for instance in instances:
         try:
-            backup_instance(instance)
+            backup_instance(instance, client)
         except:
             logging.exception('Error creating snapshot for {}'.format(instance.id))
 
 
-def backup_instance(instance):
+def backup_instance(instance, client):
     instance_tags = dict(map(lambda x: (x['Key'], x['Value']), instance.tags or []))
     instance_name = instance_tags.get('Name', '[unnamed]')
     backup_cfg_str = instance_tags['LambdaBackupConfiguration']
@@ -64,14 +72,14 @@ def backup_instance(instance):
         volume_id = device_mapping['Ebs']['VolumeId']
         device_name = device_mapping['DeviceName']
         logger.info('Create snapshot of volume {} (mounted at {})'.format(volume_id, device_name))
-        snapshot = client.create_snapshot(VolumeId=volume_id, Description='Backup of {} {}'
+        snapshot = client.create_snapshot(VolumeId=volume_id, Description='Automatic Backup of {} {}'
                                           .format(instance_name, device_name))
         snapshot_ids.append(snapshot['SnapshotId'])
 
     if snapshot_ids:
         logger.info('Create tags for snapshots {}'.format(snapshot_ids))
         tags = {
-            'Name': 'lambda-backup',
+            'Name': 'lambda-backup-' + instance_name,
             'BackupLabel': backup_label,
             'InstanceId': instance.instance_id,
             'InstanceName': instance_name,
@@ -107,7 +115,7 @@ def calc_retention(backup_configuration):
     return None, None
 
 
-def expire():
+def expire(ec2):
     delete_fmt = date.today().strftime('%Y-%m-%d')
     snapshots = ec2.snapshots.filter(OwnerIds=['self'],
                                      Filters=[{'Name': 'tag:DeleteOn', 'Values': [delete_fmt]}])
